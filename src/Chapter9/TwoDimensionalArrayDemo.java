ab7c77ad119c0c043d5de49c3b15236c07982b69
sophiay <3mayunanda@gmail.com>
2022-08-12 22:36:54 -0700

    Initial commit

---
+++ src/Chapter9/TwoDimensionalArrayDemo.java
@@ -0,0 +1,36 @@
+package Chapter9;
+
+import java.util.Scanner;
+
+public class TwoDimensionalArrayDemo {
+    public static void main(String[] args) {
+
+        int[][] count = new int [3][3];
+        int row, column;
+        final int QUIT = 999;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a row or " + QUIT + " to quit >> ");
+        row = input.nextInt();
+
+        while(row!=QUIT) {
+            System.out.println("Enter a column >> ");
+            column = input.nextInt();
+            if(row < count.length && column < count.length) {
+                count[row][column]++;
+                for (int[] r: count) {
+                    for (int c = 0; c < r.length; c++){
+                        System.out.print(r[c] + " ");
+                    }
+                    System.out.println();
+                }
+            }
+            else {
+                System.out.println("Invalid position selected.");
+            }
+            System.out.println("Enter a row or " + QUIT + " to quit >> ");
+            row = input.nextInt();
+        }
+    }
+}
